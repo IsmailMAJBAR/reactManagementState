9b6a7b1b8a5742d850a707f6cdad7d5e4c6c3dd1
majbar <majbarismail@gmail.com>
2023-09-29 00:33:00 +0200

    error component

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,19 @@ import Header from "./Header";
 import { getProducts } from "./services/productService";
 
 export default function App() {
-  // states :
   const [size, setSize] = useState("");
   const [products, setProducts] = useState([]);
-  useEffect(
-    () => getProducts("shoes").then((response) => setProducts(response)),
-    []
-  );
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    getProducts("shoes").then((response) => setProducts(response)).catch((error) => setError(error));
+  }, []);
 
   function renderProduct(p) {
     return (
-      <div
-        key={ p.id }
-        className='product'>
-        <a href='/'>
-          <img
-            src={ `/images/${ p.image }` }
-            alt={ p.name }
-          />
+      <div key={ p.id } className="product">
+        <a href="/">
+          <img src={ `/images/${ p.image }` } alt={ p.name } />
           <h3>{ p.name }</h3>
           <p>${ p.price }</p>
         </a>
@@ -30,29 +25,31 @@ export default function App() {
     );
   }
 
-  const filteredProducts = size
+  const filterProduct = size
     ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
     : products;
 
+  if (error) throw error;
+
   return (
     <>
-      <div className='content'>
+      <div className="content">
         <Header />
         <main>
-          <section id='filters'>
-            <label htmlFor='size'>Filter by Size:</label>{ " " }
-            <select
-              id='size'
-              value={ size }
-              onChange={ (e) => setSize(e.target.value) }>
-              <option value=''>All sizes</option>
-              <option value='7'>7</option>
-              <option value='8'>8</option>
-              <option value='9'>9</option>
+          <section id="filters">
+            <label htmlFor="size">Filter by Size:</label>{ " " }
+            <select id="size" value={ size }
+              onChange={ (event) => setSize(event.target.value) }>
+              <option value='' >All sizes</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
             </select>
           </section>
-          { size && <h2>Found { filteredProducts.length } items</h2> }
-          <section id='products'>{ filteredProducts.map(renderProduct) }</section>
+          { size && <h2>Found { filterProduct.length }  items</h2> }
+          <section id="products">
+            { filterProduct.map(renderProduct) }
+          </section>
         </main>
       </div>
       <Footer />
