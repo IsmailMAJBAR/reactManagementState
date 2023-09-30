80a1933e65351f4b9e93c240fd50b28b3606f692
majbar <majbarismail@gmail.com>
2023-09-30 23:06:35 +0200

    Adding sizes to detail page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import { default as React, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import PageNotFound from "./PageNotFound";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
 
+
 export default function Detail() {
+  const [sku, setSize] = useState("");
 
   const { id, category } = useParams();
   const navigate = useNavigate();
 
   const { data: product, loading, error } = useFetch("products/" + id);
+
+  function renderAvSizeOptions(p) {
+    return (
+      <option key={ p.sku } value={ p.sku }>{ p.size }</option>
+    );
+  };
+
+
 
   if (error) throw error;
 
@@ -17,13 +27,25 @@ export default function Detail() {
 
   if (!product) return <PageNotFound />;
 
+
   return (
     <div id="detail">
       <h1>{ product.name }</h1>
       <p>{ product.description }</p>
       <p id="price">${ product.price }</p>
+      <section id="filters">
+        <label htmlFor="size">Filter by Size:</label>{ " " }
+        <select id="size" value={ sku } onChange={ (event) => setSize(event.target.value) }>
+          <option value=''>what size ?</option>
+          { product.skus.map(renderAvSizeOptions) }
+        </select>
+      </section>
       <p ><button className="btn btn-primary" onClick={ () => navigate("/cart") }>Add to Cart</button></p>
       <img style={ { "width": "500px" } } src={ `/images/${ product.image }` } alt={ category } />
     </div >
+
+
+
   );
 }
+
