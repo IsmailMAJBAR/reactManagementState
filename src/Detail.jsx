05ec05e2874a53261fd914f3f97454904bd4d169
majbar <majbarismail@gmail.com>
2023-09-30 03:38:57 +0200

    detail of a product

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
 
 export default function Detail() {
-  return <h1>Detail</h1>;
 
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  const { id, category } = useParams();
+  const navigate = useNavigate();
+
+  const { data: product, loading, error } = useFetch("products/" + id);
+
+  if (error) throw error;
+
+  if (loading) return <Spinner />;
+
+  if (!product) return <PageNotFound />;
+
+  return (
+    <div id="detail">
+      <h1>{ product.name }</h1>
+      <p>{ product.description }</p>
+      <p id="price">${ product.price }</p>
+      <p ><button className="btn btn-primary" onClick={ () => navigate("/cart") }>Add to Cart</button></p>
+      <img style={ { "width": "500px" } } src={ `/images/${ product.image }` } alt={ category } />
+    </div >
+  );
 }
