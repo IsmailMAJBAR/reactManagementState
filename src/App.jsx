e7d21c1c1dd3d82e1ce10954236df7b6d05f7ba6
majbar <majbarismail@gmail.com>
2023-10-01 20:00:07 +0200

    local Storage and cart quantity

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./Cart";
@@ -10,18 +10,45 @@ import Products from "./Products";
 
 export default function App() {
 
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? []
+    } catch {
+      console.error("cart could not be parsed into json");
+      return [];
+    }
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart))
+  }, [cart]);
 
   const addToCart = (id, sku) => {
     setCart((items) => {
-      const itemInCart = items.find(i => i.sku === sku);
+      const itemInCart = items.find((i) => i.sku === sku);
       if (itemInCart) {
-        return items.map((i) => i.sku === sku ? { ...i, quantity: quantity + 1 } : i);
+        return items.map((i) =>
+          i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i);
       } else {
         return [...items, { id, sku, quantity: 1 }];
       }
     });
+  };
+
+  const updateQuantity = (sku, quantity) => {
+    setCart(
+      (items) => {
+        return quantity === 0 ? items.filter((i) => i.sku !== sku)
+          : items.map(
+            (i) => i.sku === sku ?
+              { ...i, quantity }
+              : i
+          );
+      }
+    );
   }
+
 
   return (
     <>
@@ -29,9 +56,22 @@ export default function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={ <h1>Welcome to the shop</h1> } />
-            <Route path="/:category" element={ <Products /> } />
-            <Route path="/cart" element={ <Cart /> } />
+            <Route
+              path="/"
+              element={ <h1>Welcome to the shop</h1> }
+            />
+            <Route
+              path="/:category"
+              element={ <Products /> } />
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  cart={ cart }
+                  updateQuantity={ updateQuantity }
+                />
+              }
+            />
             <Route
               path="/:category/:id"
               element={ <Detail
