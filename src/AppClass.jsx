856c7b096a1c41f7a0cee4e2691b35a28a7dbccd
majbar <majbarismail@gmail.com>
2023-10-04 02:23:46 +0200

    detail component transformed to class with hoks wraped in a function

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Cart from "./Cart";
 import Checkout from "./CheckoutClass";
-import Detail from "./Detail"; //import Detail from "./DetailRef";
+import DetailWrapper from "./DetailClass"; //import Detail from "./DetailRef";
 import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./Products";
@@ -20,7 +20,7 @@ export default function App() {
             <Route path="/" element={ <h1>Welcome to the shop</h1> } />
             <Route path="/:category" element={ <Products /> } />
             <Route path="/cart" element={ <Cart /> } />
-            <Route path="/:category/:id" element={ <Detail /> } />
+            <Route path="/:category/:id" element={ <DetailWrapper /> } />
             <Route path="/checkout" element={ <Checkout dispatch={ dispatch } /> } />
           </Routes>
         </main>
