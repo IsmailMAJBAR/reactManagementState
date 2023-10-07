5933d45a02281607fbd109b5b9e4815673ac6bd1
majbar <majbarismail@gmail.com>
2023-10-07 02:41:54 +0200

    cart reducer with immer for immutable state

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -1,16 +1,32 @@
+import { produce } from "immer";
+
 export default function cartReducer(cart, action) {
 
   switch (action.type) {
     case "empty":
       return [];
     case "updateQuantity": {
-      const { sku, quantity } = action;
-      return quantity === 0 ? cart.filter((i) => i.sku !== sku)
-        : cart.map(
-          (i) => i.sku === sku ?
-            { ...i, quantity }
-            : i
-        );
+      // IMMER for immutable state
+      const { quantity, sku } = action;
+      return produce(cart, (draft) => {
+        const index = cart.findIndex((i) => i.sku === sku);
+        if (index > -1) {
+
+          if (quantity === 0) {
+            draft.splice(index, 1);
+          } else {
+            draft[index].quantity = quantity;
+          }
+        }
+
+      });
+      // const { sku, quantity } = action;
+      // return quantity === 0 ? cart.filter((i) => i.sku !== sku)
+      //   : cart.map(
+      //     (i) => i.sku === sku ?
+      //       { ...i, quantity }
+      //       : i
+      //   );
     }
     case "addToCart":
       const { id, sku } = action;
